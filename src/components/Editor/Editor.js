d2c41e9ec50d70cf3630de42d0b1f3e43589b22e
Mike Sandula <msandula@gmail.com>
2022-08-18 14:35:28 -0400

    feat: diffEvent

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -3,8 +3,25 @@ import "./Editor.css";
 const INITIAL_TEXT = "This is a sentence. This is another.";
 
 const Editor = () => {
+  const handleInput = (event) => {
+    const { data: text, inputType: type } = event.nativeEvent;
+    const { anchorOffset: start, focusOffset: end } = document.getSelection();
+    const diffEvent = {
+      end,
+      start,
+      text,
+      type,
+    };
+    console.log("handleInput: ", diffEvent);
+  };
+
   return (
-    <div className="Editor" contentEditable suppressContentEditableWarning>
+    <div
+      className="Editor"
+      contentEditable
+      onInput={handleInput}
+      suppressContentEditableWarning
+    >
       {INITIAL_TEXT}
     </div>
   );
