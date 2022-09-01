51cd06c6da5e107c1cc5525299e9bc3dbccb9503
Mike Sandula <msandula@gmail.com>
2022-09-01 13:02:31 -0400

    feat: tests

--- src/helpers/editor.test.js
+++ src/helpers/editor.test.js
@@ -1,15 +1,15 @@
 import { updateSentences } from "./editor";
 
 jest.mock("nanoid", () => () => ({
-  nanoid: () => "some-unique-id",
+  nanoid: () => {},
 }));
 
 describe("updateSentences function", () => {
-  it("should return previous new sentences if no previous sentences", () => {
+  it("should return new sentences if no previous sentences", () => {
     const previousSentences = [];
     const newSentences = [
       {
-        id: "some-unique-id",
+        id: "some-unique-id-0",
         length: 1,
         offset: 0,
         sentence: "T",
@@ -19,5 +19,71 @@ describe("updateSentences function", () => {
     expect(updateSentences(previousSentences, newSentences, offset)).toEqual(
       newSentences
     );
+  });
+  it("should keep previous ID when updating existing sentence", () => {
+    const previousSentences = [
+      {
+        id: "some-unique-id-0",
+        length: 1,
+        offset: 0,
+        sentence: "T",
+      },
+    ];
+    const newSentences = [
+      {
+        id: "some-unique-id-1",
+        length: 2,
+        offset: 0,
+        sentence: "Th",
+      },
+    ];
+    const offset = 2;
+    expect(updateSentences(previousSentences, newSentences, offset)).toEqual([
+      {
+        id: "some-unique-id-0",
+        length: 2,
+        offset: 0,
+        sentence: "Th",
+      },
+    ]);
+  });
+  it("should handle adding a new sentence", () => {
+    const previousSentences = [
+      {
+        id: "some-unique-id-0",
+        length: 20,
+        offset: 0,
+        sentence: "This is a sentence. ",
+      },
+    ];
+    const newSentences = [
+      {
+        id: "some-unique-id-1",
+        length: 20,
+        offset: 0,
+        sentence: "This is a sentence. ",
+      },
+      {
+        id: "some-unique-id-2",
+        length: 20,
+        offset: 20,
+        sentence: "And this is one too.",
+      },
+    ];
+    const offset = 40;
+    expect(updateSentences(previousSentences, newSentences, offset)).toEqual([
+      {
+        id: "some-unique-id-0",
+        length: 20,
+        offset: 0,
+        sentence: "This is a sentence. ",
+      },
+      {
+        id: "some-unique-id-2",
+        length: 20,
+        offset: 20,
+        sentence: "And this is one too.",
+      },
+    ]);
   });
 });
